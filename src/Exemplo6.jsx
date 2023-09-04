2db9436f40d62fb21134f99c3d440748a8029203
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-04 11:44:31 -0300

    vercel

---
+++ src/Exemplo6.jsx
@@ -0,0 +1,10 @@
+import {} from 'react'
+
+function Exemplo6(){
+    return(
+        <>
+        <h1>Exemplo 6</h1>
+        </>
+    )
+}
+export default Exemplo6
---
+++ src/Nav.jsx
@@ -0,0 +1,16 @@
+import {} from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Nav(){
+    return(
+        <>
+        <NavLink to='Exemplo1'>Exemplo 1</NavLink>
+        <NavLink to='Exemplo2'>Exemplo 2</NavLink>
+        <NavLink to='Exemplo3'>Exemplo 3</NavLink>
+        <NavLink to='Exemplo4'>Exemplo 4</NavLink>
+        <NavLink to='Exemplo5'>Exemplo 5</NavLink>
+        <NavLink to='Exemplo6'>Exemplo 6</NavLink>
+        </>
+    )
+}
+export default Nav
